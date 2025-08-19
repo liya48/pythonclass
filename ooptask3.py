2597907ff637104077e37b50ea473ee59bfd57e6
liya48 <liyafathimap@gmail.com>
2025-08-19 11:36:16 +0530

    n

--- ooptask3.py
+++ ooptask3.py
@@ -14,12 +14,28 @@
 # make_sound(parrot)
 # make_sound(sparrow)
 
-#4
-# class Book:
-#     def __init__(self,title,author,copies):
-#         self.title=title
-#         self.author=author
-#         self.copies=copies
-#     def display_info(self):
-#         print(f"TITLE:{self.title}\nAUTHOR:{self.author}\nCOPIES AVAILABLE:{self.copies}")
+# 4
+class Book:
+    def __init__(self,title,author,copies):
+        self.title=title
+        self.author=author
+        self.copies=copies
+    def display_info(self):
+        print(f"TITLE:{self.title}\nAUTHOR:{self.author}\nCOPIES AVAILABLE:{self.copies}")
+books=[]
+for i in range(3):
+    title=input("enter the book title:")
+    author=input("enter author:")
+    copies=int(input("enter copies:"))
+    book=Book(title,author,copies)
+    books.append(book)
+search_title=input("enter title to search")
+flag=0
+for book in books:
+    if (book.title.lower()==search_title.lower()):
+        book.display_info()
+        flag=1
+        break
+if flag==0:
+    print("book not found")
     
